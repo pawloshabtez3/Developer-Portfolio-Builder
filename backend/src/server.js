31f46591bf2da321566bf8614874cee847e2a4ec
pawloshabtez3 <pawloshabtez@gmail.com>
2026-02-12 14:19:48 +0300

    feat: Update server listening address and enhance PublicPortfolioPage layout

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDb()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
